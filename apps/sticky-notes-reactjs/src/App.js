09427777ef7f21ab077ba78f11f1255ab2e0d87e
lrb7dev <luanrb.dev@gmail.com>
2022-11-29 17:39:13 -0300

    new

--- apps/sticky-notes-reactjs/src/App.js
+++ apps/sticky-notes-reactjs/src/App.js
@@ -45,7 +45,7 @@ function App() {
     }
 
     dispatch({ type: 'ADD_NOTE', payload: newNote })
-    setNoteInput(' ') //empty
+    setNoteInput('') //empty
   }
 
   const dropNote = event => {
@@ -82,6 +82,13 @@ function App() {
             key={note.id}
             >
             
+            <div className='close'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+
+            </div>
+
             <pre className='text'>{note.text}</pre>
           </div>
         ))
